ed1fde5b3cec527b27c8fa9c554bcfa94b8cc3f5
masterzorag <masterzorag@gmail.com>
2015-06-12 17:12:44 +0200

    cleanup

--- src/xbm_print.c
+++ src/xbm_print.c
@@ -6,31 +6,29 @@
 
 #include "trfb.h"
 
+/* generate with genXBMfonts, https://github.com/masterzorag/xbm_tools */
+#include "xbm_font.h"
+
 /*
 	Printing a text with xbm
 */
-#include "xbm_font.h" /* ! generate it with my genXBMfonts script ! */
-
 void xbm_print(int x, const int y, const char *text, trfb_framebuffer_t *fb)
 {
 	short i, j;
-	// 2 ^16 = 65536, sqrt(65536) = 256
+	// 2^16 = 65536, sqrt(65536) = 256
 	// 256*256 px max with a short !
-	int tx = 0;
-	int ty = 0;
+	int tx = 0,	ty = 0;
 	char c;
-	short n_char8 = (FONT_W * FONT_H) / BITS_IN_BYTE;
 
 	while(*text != '\0'){
 		c = *text;
-
 		if(c < LOWER_ASCII_CODE || c > UPPER_ASCII_CODE) c = 180;
 		
 		// indexing font glyph by current char
 		char *bit = xbmFont[c - LOWER_ASCII_CODE];
 		
 		// dump bits map: bytes_per_line 2, size 32 char of 8 bit
-		for(i = 0; i < n_char8; i++) {
+		for(i = 0; i < ((FONT_W * FONT_H) / BITS_IN_BYTE); i++) {
 			for(j = 0; j < BITS_IN_BYTE; j++){
 				//	printf("%c", (bit[i] & (1 << j)) ? ' ' : '0' );		// least significant bit first
 				//	printf("%d", (bit[i] & (0x80 >> j)) ? 1 : 0); 			// right shifting the value will print bits in reverse.				
@@ -42,7 +40,7 @@ void xbm_print(int x, const int y, const char *text, trfb_framebuffer_t *fb)
 						 40 - ty*3 + (tx * BITS_IN_BYTE + j) *4
 						));
 				}
-				else // paint BG pixel, (or trasparency !)
+				else // paint BG pixel (or trasparency)
 				{
 					trfb_framebuffer_set_pixel(fb, x + tx * BITS_IN_BYTE + j, y + ty, TRFB_RGB(0, 0, 0));
 				}
